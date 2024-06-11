c12a597be65c4b3fbb8b88e92e0bfc9431caaa0e
Fabiano <fasnaweb2020@gmail.com>
2024-06-11 20:14:04 -0300

    Bora continuar

--- Dow/index.js
+++ Dow/index.js
@@ -14,8 +14,13 @@ const btnMenosLanchinho = document.querySelector('.btn-menos-lanchinho');
 const qtdLanchinho = document.querySelector('#qtd-lanchinho');
 const btnMaisLanchinho = document.querySelector('.btn-mais-lanchinho');
 
+const bntMenosOvos = document.querySelector('.btn-menos-ovo');
+const qtdOvos = document.querySelector('#qtd-ovo');
+const bntMaisOvos = document.querySelector('.btn-mais-ovo');
 
-
+const btnMenosAbacaxi = document.querySelector('.btn-menos-abacaxi');
+const qtdAbacaxi = document.querySelector('#qtd-abacaxi');
+const btnMaisAbacaxi = document.querySelector('.btn-mais-abacaxi');
 
 
 const atualizarQuantidade = (btnMenos, btnMais, qtElementos) => {
@@ -34,6 +39,9 @@ atualizarQuantidade(bntMenosLanchao, btnMaisLanchao, qtdLanchao);
 atualizarQuantidade(btnMenosLanche, btnMaisLanche, qtdLanche);
 atualizarQuantidade(btnMenosLanchinho, btnMaisLanchinho, qtdLanchinho);
 
+atualizarQuantidade(bntMenosOvos, bntMaisOvos, qtdOvos);
+atualizarQuantidade(btnMenosAbacaxi, btnMaisAbacaxi, qtdAbacaxi);
+
 form.addEventListener('submit', (event) => {
     event.preventDefault();
 
@@ -41,5 +49,11 @@ form.addEventListener('submit', (event) => {
 
     orderInfo.Nome = inputName.value;
     orderInfo.Email = inputEmail.value;
+
+    if(parseInt(qtdLanchao.innerText) > 0) orderInfo.lanchao = parseInt(qtdLanchao.innerText);
+    if(parseInt(qtdLanche.innerText) > 0) orderInfo.lanche = parseInt(qtdLanche.innerText);
+    if(parseInt(qtdLanchinho.innerText) > 0) orderInfo.lanchinho = parseInt(qtdLanchinho.innerText);
+    if(parseInt(qtdOvos.innerText) > 0) orderInfo.Ovos = parseInt(qtdOvos.innerText);
+    if(parseInt(qtdAbacaxi.innerText) > 0) orderInfo.Abacaxi = parseInt(qtdAbacaxi.innerText);
     console.log(orderInfo);
 })
